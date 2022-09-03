f0752eaf8b438430f9d1545c1ebece0b6310e7dc
Alexander <atvcreative@gmail.com>
2022-09-03 15:33:06 +0300

    Updates and new header

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -2,26 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// mount function to start app
-
+// Mount function to start up the app
 const mount = (el) => {
-    ReactDOM.render(
-        <App />,
-        el
-    )
-}
+  ReactDOM.render(<App />, el);
+};
 
-// if we are in dev
-// call mount
-
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_marketing-dev-root');
 
-    if (devRoot) {
-        mount(devRoot);
-    }
+  if (devRoot) {
+    mount(devRoot);
+  }
 }
-// we are runnign in container
-// we should export  the mount function
 
+// We are running through container
+// and we should export the mount function
 export { mount };
